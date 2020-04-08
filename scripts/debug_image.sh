800022b348076401a6bc217dd63274597ab28e61
TJ Maynes (Personal) <tj@tjmaynes.com>
2020-04-08 15:15:26 -0400

    Remove database env requirements

--- scripts/debug_image.sh
+++ scripts/debug_image.sh
@@ -18,12 +18,6 @@ elif [[ -z $TAG ]]; then
 elif [[ -z $DATABASE_URI ]]; then
     echo "Please provide a uri for accessing the database."
     exit 1
-elif [[ -z $DATABASE_MIGRATIONS_DIR ]]; then
-    echo "Please provide a migrations directory."
-    exit 1
-elif [[ -z $DATABASE_SCHEMA_FILE ]]; then
-    echo "Please provide a scheme file location."
-    exit 1
 elif [[ -z $FLASK_APP ]]; then
     echo "Please provide an entrypoint to the api service."
     exit 1
@@ -31,8 +25,6 @@ fi
 
 docker run --rm \
     --env DATABASE_URI=$DATABASE_URI \
-    --env DATABASE_MIGRATIONS_DIR=$DATABASE_MIGRATIONS_DIR \
-    --env DATABASE_SCHEMA_FILE=$DATABASE_SCHEMA_FILE \
     --env FLASK_APP=$FLASK_APP \
     --publish 5000:5000 \
      $REGISTRY_USERNAME/$IMAGE_NAME:$TAG
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,12 +5,6 @@ set -e
 if [[ -z $DATABASE_URI ]]; then
     echo "Please provide a uri for accessing the database."
     exit 1
-elif [[ -z $DATABASE_MIGRATIONS_DIR ]]; then
-    echo "Please provide a migrations directory."
-    exit 1
-elif [[ -z $DATABASE_SCHEMA_FILE ]]; then
-    echo "Please provide a scheme file location."
-    exit 1
 elif [[ -z $FLASK_APP ]]; then
     echo "Please provide an entrypoint to the api service."
     exit 1
@@ -21,8 +15,8 @@ DATABASE_URL=$DATABASE_URI dbmate wait
 
 # Run migrations
 DATABASE_URL=$DATABASE_URI dbmate \
---migrations-dir $DATABASE_MIGRATIONS_DIR \
---schema-file $DATABASE_SCHEMA_FILE \
+--migrations-dir cart_database/migrations \
+--schema-file cart_database/schema.sql \
 up
 
 python cart_api/api_test.py
